cf1b46ecb673810517f67303553ecd644d27de89
yusuf erdogan <a.erdogan2018@gtu.edu.tr>
2021-08-12 11:49:18 +0300

    Primary Bean added When no Qualifier annotation used Spring Uses this primary Controller its like default bean

--- src/main/java/guru/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -1,11 +1,18 @@
 package guru.springframework.sfgdi.controllers;
 
+import guru.springframework.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String sayHello(){
-        System.out.println("MyController");
-        return "Hello - MyController";
+        return greetingService.sayGreeting();
     }
 }
