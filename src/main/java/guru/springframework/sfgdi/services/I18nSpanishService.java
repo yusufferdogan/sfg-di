d7280738d4aa8c09d9c651335bd1c3fc32fd461e
yusuf erdogan <a.erdogan2018@gtu.edu.tr>
2021-08-12 22:26:24 +0300

    Default Profile Used.When we specified default in class we do not need to show in application.properties

--- src/main/java/guru/springframework/sfgdi/services/I18nSpanishService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishService.java
@@ -2,7 +2,7 @@ package guru.springframework.sfgdi.services;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-@Profile("ES")
+@Profile({"ES","default"})
 @Service("i18nService")
 public class I18nSpanishService implements GreetingService{
     @Override
